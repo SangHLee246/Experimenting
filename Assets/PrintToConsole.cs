bbcdd65bd7b203d1c3b15da70c6b9aa100819db8
SangHLee246 <akayasha1234@yahoo.com>
2021-04-15 21:02:09 -0400

    Initial commit

---
+++ Assets/PrintToConsole.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I'm a tank. Become the tank. Become one");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
